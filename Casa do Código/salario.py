606dfdd3c00306aedc954f8072d48d92a1e1a6f4
Cayo Eduardo <cayoesn@gmail.com>
2020-01-10 20:19:32 -0300

    ...

--- Casa do Código/salario.py
+++ Casa do Código/salario.py
@@ -1,3 +1,9 @@
-salario = int(input('Salario? '))
-imposto = float(input('Imposto em % (ex: 27.5)? '))
-print("Valor real: {0}".format(salario - (salario * (imposto * 0.01))))
+salario = int(input("Salario ? "))
+imposto = input("Imposto em % (ex: 27.5) ? ")
+
+if not imposto:
+    imposto = 27.5
+else:
+    imposto = float(imposto)
+
+print("Valor real: R$ {0}".format(salario - (salario * (imposto / 100))))
